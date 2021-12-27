53f20d3779e42e942323d53eac1eda405a64f35a
Anahi Baca <anahi88.bz@gmail.com>
2021-12-26 19:48:50 -0500

    Añadiendo enlaces y menu desplegable con javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+const $detailsList = document.querySelectorAll('details')
+
+$detailsList.forEach(($details)=> {
+    $details.querySelector('summary').addEventListener('click', expand)
+
+})
+
+function expand () {
+    $detailsList.forEach(($details)=> {
+        $details.removeAttribute('open')
+    }) 
+}
